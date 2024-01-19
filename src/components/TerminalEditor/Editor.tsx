20ec2c5aae876b3906831cbbd3237619e881b02f
madhav-fied <madhavcv2116@gmail.com>
2024-01-20 00:50:01 +0530

    feat(common): add terminal component

--- src/components/TerminalEditor/Editor.tsx
+++ src/components/TerminalEditor/Editor.tsx
@@ -1,7 +1,9 @@
-import {EditorContent, useEditor} from '@tiptap/react';
+import { EditorContent, useEditor } from '@tiptap/react';
 import Document from '@tiptap/extension-document'
 import Paragraph from '@tiptap/extension-paragraph'
 import Text from '@tiptap/extension-text'
+
+import { Node } from '@tiptap/core'
 
 import styled from 'styled-components';
 import { useState } from 'react';
@@ -19,30 +21,95 @@ const EditorContainer = styled.div`
     }
 `;
 
+declare module '@tiptap/core' {
+    interface Commands<ReturnType> {
+        customExtension: {
+            toggleTerminal: () => ReturnType,
+            clearTerminal: () => ReturnType,
+            execute: () => ReturnType,
+        }
+    }
+}
+
+export interface CLIOptions {
+    // for custom styling
+    HTMLAttributes: Record<string, any>,
+}
+
+const CLI = Node.create<{}, CLIOptions>({
+    name: 'CLI',
+    content: 'text*',
+    marks: '',
+    group: 'block list',
+
+    // isolating: true,
+    // defining: true,
+
+    renderHTML({ HTMLAttributes }) {
+        return ['pre', 'vasudevn@hyd008:~$ ', ['code', HTMLAttributes, 0],]
+    },
+
+    addCommands() {
+        return {
+            toggleTerminal:
+                () => ({ commands }) => {
+                    if (!this.editor.isActive('CLI') || this.editor.isEmpty) {
+                        console.log('currently inactive')
+                        return commands.toggleNode(this.name, 'paragraph');
+                    }
+                    console.log('currently active');
+                    return true;
+                },
+            clearTerminal:
+                () => ({ }) => {
+
+                    this.editor.chain().focus().clearContent().toggleTerminal().run();
+                    return true;
+                },
+            execute:
+                () => ({ }) => {
+                    this.editor.chain().focus().clearContent().toggleTerminal().run();
+                    return true;
+                }
+        }
+    },
+
+    addKeyboardShortcuts() {
+        return {
+            'Alt-Enter': () => this.editor.commands.toggleTerminal(),
+            'Mod-Alt-l': () => this.editor.commands.clearTerminal(),
+            'Enter': () => this.editor.commands.execute(),
+        }
+    },
+
+})
+
 
 const TerminalEditor = () => {
-
-    const [content, setContent] = useState<string | undefined>('');
-
-
-    function handleUpdate() {
-        const html = editor?.getHTML();
-        setContent(html);
-    }
 
     const editor = useEditor({
         extensions: [
             Document,
             Paragraph,
             Text,
+            CLI,
         ],
-        onUpdate: handleUpdate,
+        onUpdate() {
+            editor?.commands.toggleTerminal();
+        },
+
+        onCreate() {
+            editor?.commands.toggleTerminal();
+        },
+
+        onFocus() {
+            editor?.commands.toggleTerminal();
+        },
     });
-    
+
     return (
         <EditorContainer>
             <EditorContent editor={editor} />
-            <h4>{content}</h4>
         </EditorContainer>
     )
 }
